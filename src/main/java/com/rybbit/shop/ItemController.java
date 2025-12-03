ebc9916c432a5e446eb7d4f589ec06b888076298
rybbit <rybbit3@gmail.com>
2025-12-03 16:36:24 +0900

    상품 추가기능 연습(미완)

--- src/main/java/com/rybbit/shop/ItemController.java
+++ src/main/java/com/rybbit/shop/ItemController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -22,5 +23,27 @@ public class ItemController {
         System.out.println(a.getTitle());
 
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(String title, String price) {
+
+        // 1. 빈 Item 객체 생성 (과제 힌트: new Item())
+        Item item = new Item();
+
+        // 2. 받아온 데이터를 객체에 집어넣기
+        item.title = item.getTitle();
+        item.price = item.getPrice();
+
+        // 3. Repository를 통해 DB에 저장 (과제 힌트: save())
+        itemRepository.save(item);
+
+        // 4. 저장이 끝나면 메인 페이지 등으로 이동
+        return "redirect:/list";
     }
 }
